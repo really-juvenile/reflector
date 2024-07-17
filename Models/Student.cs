1e49b65226610f826271823906143c4eaaef3009
Arjun Pawar <Arjun.Pawar@aurionpro.com>
2024-07-17 20:39:42 +0530

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reflector.Models
+{
+    internal class Student
+    {
+        public int RollNo { get; set; }
+        public string Name { get; set; }
+
+       
+        public Student()
+        {
+
+        }
+
+        
+        public Student(int rollno, string name)
+        {
+            RollNo = rollno;
+            Name = name;
+        }
+
+        
+        public void displayData()
+        {
+            Console.WriteLine("helllo");
+           
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using reflector.Models;
+using System.Reflection;
+
+namespace reflector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(1, "Allen");
+            showDetails(student);
+
+
+
+        }
+        //Assembly executing = Assembly.GetExecutingAssembly();
+
+        //Type[] types = executing.GetTypes();
+        //    foreach (string item in types)
+        static void showDetails(object obj)
+        {
+            Type types = obj.GetType();
+            Console.WriteLine("Class: {0}", types.Name);
+               MethodInfo[] methods =types.GetMethods();
+
+            Console.WriteLine("No. of Methods: {0}",methods.Length) ;
+
+            PropertyInfo[] properties =types.GetProperties();
+
+
+            Console.WriteLine("No. of Properties: {0}",properties.Length);
+
+           ConstructorInfo[] constructors=types.GetConstructors() ;
+            Console.WriteLine("no. of Constructors: {0}",   constructors.Length);
+        }
+    }
+}
